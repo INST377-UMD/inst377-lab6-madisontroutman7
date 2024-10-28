c54bfda1e5414cf303992e459ea37593ec577fc9
madisontroutman7 <madison4@terpmail.umd.edu>
2024-10-27 22:03:10 -0400

    commit

--- mapping.js
+++ mapping.js
@@ -1,22 +1,33 @@
-
 function createMap() {
-var map = L.map('map').setView([38.98, -76], 13);
-
+    var map = L.map('map').setView([39.50, -98.35], 4); 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
-    var marker = L.marker([51.5, -0.09]).addTo(map);
+    const cord = [];
+    for (let m = 1; m <= 3; m++) {
+        const latitude = getRandomInRange(30, 35, 3);
+        const longitude = getRandomInRange(-90, -100, 3);
+        cord.push({ latitude, longitude });  // store coordinate 
+        const marker = L.marker([latitude, longitude]).addTo(map); // Marker 
+
+        // Fetch locality
+        fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`)
+            .then(response => response.json())
+            .then(data => {
+                const locality = data.locality; // locality
+                document.getElementById(`mark${m}`).innerHTML = `
+                    <b>Marker: ${m}</b>: Latitude: ${latitude}, Longitude: ${longitude} <br> Locality: ${locality} <br>
+                `;
+            });
+    }
+    map.fitBounds(cord.map(c=>[c.latitude + 1, c.longitude + 1])); //fixing view to focus on markers
+}
+function getRandomInRange(from, to, fixed) { // random coordinates in range
+    return (Math.random() * (to - from) + from).toFixed(fixed) * 1;
 }
 
 window.onload = createMap;
-
-
-function getRandomInRange(from, to, fixed) {
-    return (Math.random() * (to - from) + from).toFixed(fixed) * 1;
-    // .toFixed() returns string, so ' * 1' is a trick to convert to number
-    }
     
     
 
